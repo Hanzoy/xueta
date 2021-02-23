44512bc2c1fc16f5aeade4f8258cbd7d7a1f85d4
大小姐 <60333385+Hanzoy@users.noreply.github.com>
2021-02-23 23:50:39 +0800

    init

---
+++ src/main/java/com/hanzoy/xueta/XuetaApplication.java
@@ -0,0 +1,13 @@
+package com.hanzoy.xueta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XuetaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XuetaApplication.class, args);
+    }
+
+}
